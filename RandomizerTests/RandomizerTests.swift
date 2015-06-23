0ceaec18357add296cf0df2b4e20136d915daf47
Bryce Young <bryceyoung@Rebeccas-MacBook-Pro.local>
2015-06-22 23:15:40 -0700

    Initial Commit

---
+++ RandomizerTests/RandomizerTests.swift
@@ -0,0 +1,35 @@
+//
+//  RandomizerTests.swift
+//  RandomizerTests
+//
+//  Created by Bryce Young on 6/22/15.
+//  Copyright © 2015 Bryce Young. All rights reserved.
+//
+
+import XCTest
+
+class RandomizerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
